8ecbe29f88df46a12b61c4182f638d5e4b15f60b
Bilal Haidar <bhaidar@gmail.com>
2019-12-24 16:25:19 +0200

    Add old test

--- cypress/integration/salesforce-contactus.spec.js
+++ cypress/integration/salesforce-contactus.spec.js
@@ -6,7 +6,6 @@ context('Applitools', () => {
         // Open the Applitools Test
         cy.eyesOpen({
             appName: 'Applitools CircleCI',
-            batchName: 'Circle CI batch',
             browser: [
                 { width: 800, height: 600, name: 'firefox' },
                 { width: 1024, height: 768, name: 'chrome' },
@@ -28,64 +27,61 @@ context('Applitools', () => {
         cy.location('pathname', { timeout: 10000 })
             .should('include', '/contactme');
 
-        cy.get('button[id="onetrust-accept-btn-handler"]', { timeout: 10000 })
-            .click();
-
         // Take a snapshot of the contact form
         cy.eyesCheckWindow('Contact Form');
 
         // Fill First Name
-        cy.get('input[id^="UserFirstName"]')
+        cy.get('input[id="reg_form_1-UserFirstName"]')
             .type('John')
             .should('have.value', 'John');
 
         // Fill Last Name
-        cy.get('input[id^="UserLastName"]')
+        cy.get('input[id="reg_form_1-UserLastName"]')
             .type('Smith')
             .should('have.value', 'Smith');
 
         // Fill User Title
-        cy.get('input[id^="UserTitle"]')
+        cy.get('input[id="reg_form_1-UserTitle"]')
             .type('Senior Manager')
             .should('have.value', 'Senior Manager');
 
         // Fill a email address
-        cy.get('input[id^="UserEmail"]')
+        cy.get('input[id="reg_form_1-UserEmail"]')
             .type('john.smith@gmail.com')
             .should('have.value', 'john.smith@gmail.com');
 
         // Fill a Phone number
-        cy.get('input[id^="UserPhone"]')
+        cy.get('input[id="reg_form_1-UserPhone"]')
             .type('+1-202-555-0176')
             .should('have.value', '+1-202-555-0176');
 
         // Fill a Company Name
-        cy.get('input[id^="CompanyName"]')
+        cy.get('input[id="reg_form_1-CompanyName"]')
             .type('John Smith LTD')
             .should('have.value', 'John Smith LTD');
 
         // Select number of employees
-        cy.get('select[id^="CompanyEmployees"]')
+        cy.get('select[id="reg_form_1-CompanyEmployees"]')
             .select('3')
             .should('have.value', '3');
 
         // Select Country
-        cy.get('select[id^="CompanyCountry"]')
+        cy.get('select[id="reg_form_1-CompanyCountry"]')
             .select('US')
             .should('have.value', 'US');
 
         // Select State
-        cy.get('select[id^="CompanyState"]')
+        cy.get('select[id="reg_form_1-CompanyState"]')
             .select('AZ')
             .should('have.value', 'AZ');
 
         // Select Product/Interest
-        cy.get('select[id^="Lead.Primary_Product_Interest"]')
+        cy.get('select[id="reg_form_1-Lead.Primary_Product_Interest__c"]')
             .select('Sales')
             .should('have.value', 'Sales');
 
         // Fill the User Description
-        cy.get('textarea[id^="UserDescription"]')
+        cy.get('textarea[id="reg_form_1-UserDescription"]')
             .type('I am requesting a demo!')
             .blur()
             .should('have.value', 'I am requesting a demo!');
